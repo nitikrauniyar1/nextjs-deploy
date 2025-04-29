ec7d34d790084d951a045195f0a361d82e681a46
rauniyar-diksha <rauniyardiksha3@gmail.com>
2025-04-30 00:30:46 +0545

    Update layout: Added Microsoft Clarity script and changed favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    
+      <head>
+        {/* Microsoft Clarity Tracking Script */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "rbuvvwi1aq");`,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
